c69a0f10d732c8dc011315cd7e2359ad2660f1fa
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-10-03 15:34:27 -0300

    🌍 Openning the web

--- tasks.py
+++ tasks.py
@@ -1,6 +1,14 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
 def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+    """Insert the sales data for the week and export it as a PDF"""
+    browser.configure(
+        slowmo=1000,
+    )
+    open_the_intranet_website() #function responsible to access the intranet
+
+def open_the_intranet_website():
+    """Navigates to the given URL"""
+    browser.goto("https://robotsparebinindustries.com/")
