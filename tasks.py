41c73c80e5a7b8dd193769144b38bd4c82f1bd1e
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-10-03 15:47:30 -0300

    ⚓ filling the fields

--- tasks.py
+++ tasks.py
@@ -2,13 +2,21 @@ from robocorp.tasks import task
 from robocorp import browser
 
 @task
-def minimal_task():
+def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
     browser.configure(
-        slowmo=1000,
+        slowmo=100,
     )
-    open_the_intranet_website() #function responsible to access the intranet
+    open_the_intranet_website()
+    log_in()
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
     browser.goto("https://robotsparebinindustries.com/")
+
+def log_in():
+    """Fills in the login form and clicks the 'Log in' button"""
+    page = browser.page()
+    page.fill("#username", "maria")
+    page.fill("#password", "thoushallnotpass")
+    page.click("button:text('Log in')")
