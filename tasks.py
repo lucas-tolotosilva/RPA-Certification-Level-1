9796636df6992e94e39420329e1235b19452d2a3
lucas-tolotosilva <lucastoloto.silva@gmail.com>
2023-10-03 15:59:01 -0300

    Downloading the Excel

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from robocorp.tasks import task
 from robocorp import browser
 
+from RPA.HTTP import HTTP
+
 @task
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
@@ -9,6 +11,8 @@ def robot_spare_bin_python():
     )
     open_the_intranet_website()
     log_in()
+    download_excel_file()
+    fill_and_submit_sales_form()
 
 def open_the_intranet_website():
     """Navigates to the given URL"""
@@ -20,3 +24,18 @@ def log_in():
     page.fill("#username", "maria")
     page.fill("#password", "thoushallnotpass")
     page.click("button:text('Log in')")
+
+def fill_and_submit_sales_form():
+    """Fills in the sales data and click the 'Submit' button"""
+    page = browser.page()
+
+    page.fill("#firstname", "John")
+    page.fill("#lastname", "Smith")
+    page.fill("#salesresult", "123")
+    page.select_option("#salestarget", "10000")
+    page.click("text=Submit")
+
+def download_excel_file():
+    """Downloads excel file from the given URL"""
+    http = HTTP()
+    http.download(url="https://robotsparebinindustries.com/SalesData.xlsx", overwrite=True)
